0656bbc51938747768e5deb4711e19c958de7f8e
Lee <leeblazek@gmail.com>
2016-01-18 20:10:55 -0700

    added styles for each module

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ require('oclazyload');
 require('./page3/page3Module')(Angular);
 //require('./page4Module')(Angular);
 
+require('./styles/main.scss')
 const ngModule = Angular.module('myApp', [require('angular-ui-router'), 'oc.lazyLoad', 'page3App']);
 
 require('./config')(ngModule, Angular);
--- app/page3/page3Module.js
+++ app/page3/page3Module.js
@@ -1,5 +1,5 @@
 export default Angular => {
     const ngPage3Module = Angular.module('page3App', []);
-
+    require('./page3.scss');
     require('./page3Controller')(ngPage3Module);
 }
